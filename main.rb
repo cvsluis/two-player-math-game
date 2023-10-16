c650ee4954093a279c326ed5e0ee32dc12f52bde
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-10-15 21:29:04 -0700

    fix: refactor player input string

--- main.rb
+++ main.rb
@@ -5,11 +5,11 @@ puts "Answer the addition question correctly or else lose one of 3 lives!"
 
 puts "Player 1, what is your name?"
 player_one = gets.chomp
-player_one = player_one == '' || player_one == ' ' ? "Player 1" : player_one
+player_one = player_one.strip == '' ? "Player 1" : player_one
 
 puts "Player 2, what is your name?"
 player_two = gets.chomp
-player_two = player_two == '' || player_one == ' ' ? "Player 2" : player_two
+player_two = player_two.strip == '' ? "Player 2" : player_two
 
 puts "#{player_one} starts!"
 
